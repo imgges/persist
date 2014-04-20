657ea31a8373be43b33d80cfe58915e294ec21ea
Stephen Tudor <smtudor@gmail.com>
2014-04-19 22:19:24 -0400

    Add a more useful comment

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 var persist = require('./lib/persist');
 
+/**
+ * @param String db MongoDB connection string
+ * @param Array data
+ * @return promise
+ * This module makes use of the node-promise API.
+ * Operate on the singular result argument passed to a `then` callback, as follows:
+ *
+ * persist(db, data).then(function (result) {
+ *   // Operate on the mongodb documents
+ * }, function (error) {
+ *   // Handle errors
+ * })
+ */
 module.exports = persist;
